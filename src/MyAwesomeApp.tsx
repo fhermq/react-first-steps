d4d4f72bb4a41cb26dfb9ba7399053d1fe0e7092
Fernando Mirasol <fhermq@gmail.com>
2025-09-22 13:28:32 -0600

    Se agrega una funcion

---
+++ src/MyAwesomeApp.tsx
@@ -0,0 +1,19 @@
+//Utilizar funcion de flecha.
+export const MyAwesomeApp = () =>{
+     return (
+        <>
+            <h1>Fernando =O</h1>
+            <h3>Mirasol</h3>
+        </>
+    )
+}
+
+
+// export function MyAwesomeApp() {
+//     return (
+//         <>
+//             <h1>Fernando =)</h1>
+//             <h3>Mirasol</h3>
+//         </>
+//     )
+// }
