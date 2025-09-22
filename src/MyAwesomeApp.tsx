fe1eecaf3ed270a91391bf752cf3c06a30986447
Fernando Mirasol <fhermq@gmail.com>
2025-09-22 13:39:03 -0600

    Se agregan constantes, arreglos y objetos

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,9 +1,23 @@
+ const firstName = 'Fernando :)';
+    const lastName = 'Mirasol';
+
+    const favoriteGames = ['uno', 'dos', 'tres'];
+    const isActive = false; //No genera nada en react
+
+    const address = {
+        zipCode: '03340',
+        country: 'México'
+    }
+
 //Utilizar funcion de flecha.
-export const MyAwesomeApp = () =>{
-     return (
+export const MyAwesomeApp = () => {
+    return (
         <>
-            <h1>Fernando =O</h1>
-            <h3>Mirasol</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+            <p>{favoriteGames.join(', ')}</p>
+            <h1>{isActive? 'Activo' :'No activo'}</h1>
+            <p>{JSON.stringify(address)}</p>
         </>
     )
 }
